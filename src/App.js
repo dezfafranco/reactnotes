c79e5fbbe7a71b9e2c2aa64dbaf3153832ee667e
Franco <dezfafranco@gmail.com>
2018-01-16 16:54:01 -0600

    Commit inicial del proyecto

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Note from './Note/Note.jsx';
+import NoteForm from './NoteForm/NoteForm.jsx';
+
+import firebase from 'firebase';
+import { DB_CONFIG } from './config/config.js';
+import 'firebase/database';
+
+class App extends Component {
+
+constructor() {
+  super();
+  this.state = {
+    notes: [
+    //{noteId: 1, noteContent: 'note 1'},
+    //{noteId: 2, noteContent: 'note 2'}
+    ]
+  };
+  //db connection
+  this.app = firebase.initializeApp(DB_CONFIG);
+  this.db = this.app.database().ref().child('notes');
+
+  this.addNote = this.addNote.bind(this);
+}
+
+componentDidMount() {
+  const { notes } = this.state;
+  this.db.on('child_added', snap => {
+    notes.push({
+      noteId: snap.key,
+      noteContent: snap.val().noteContent
+    })
+    this.setState({notes});
+  });
+}
+
+removeNote() {
+
+}
+
+addNote(note) {
+  /*
+  let {notes} = this.state;
+  notes.push ({
+    noteId: notes.length + 1,
+    noteContent: note
+  });
+  this.setState({
+    notes
+  })
+  */
+  this.db.push().set({noteContent:note});
+}
+
+  render() {
+    return (
+      <div className="notesContainer">
+
+          <div className="notesHeader">
+          <h1> React y Firebase App :V</h1>
+          </div>
+
+          <div className="notesBody">
+          <ul>
+             {
+              this.state.notes.map(note => {
+                  return (
+                   <Note
+                      noteContent={note.noteContent}
+                      noteId={note.noteId}
+                      key={note.noteId}
+                   />
+               )
+              })
+             }
+             </ul>
+          </div>
+          <div className="notesFooter">
+              <NoteForm addNote={this.addNote}/>
+          </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+export const DB_CONFIG = {
+  apiKey: "AIzaSyC5blK-nRPGb8DvZs6IOn9lp-qyTVALIQA",
+   authDomain: "notesreact-b8a0b.firebaseapp.com",
+   databaseURL: "https://notesreact-b8a0b.firebaseio.com",
+   projectId: "notesreact-b8a0b",
+   storageBucket: "",
+   messagingSenderId: "880165753686"
+};
